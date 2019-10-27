7a84bc8f052c5177f6228fc677c53807af01dea7
rishabhydv <rishabhydv@gmail.com>
2019-10-27 03:44:03 -0400

    test

---
+++ ios/Flutter/flutter_export_environment.sh
@@ -0,0 +1,8 @@
+#!/bin/sh
+# This is a generated file; do not edit or check into version control.
+export "FLUTTER_ROOT=/Users/rishabh/code/network/flutter"
+export "FLUTTER_APPLICATION_PATH=/Users/rishabh/Code/jetblue"
+export "FLUTTER_TARGET=lib/main.dart"
+export "FLUTTER_BUILD_DIR=build"
+export "SYMROOT=${SOURCE_ROOT}/../build/ios"
+export "FLUTTER_FRAMEWORK_DIR=/Users/rishabh/code/network/flutter/bin/cache/artifacts/engine/ios"
